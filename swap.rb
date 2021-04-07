a67216fd6bd2ccb9cb019bc2d53cc0092e002fd8
lukasikb <lukasikbed@onet.pl>
2021-04-07 20:55:55 +0200

    Swapping word's first letter with the last letter and vice versa

--- swap.rb
+++ swap.rb
@@ -1,8 +1,7 @@
 print "Input any word: "
 word = gets.chomp
 first_letter = word[0]
-number_of_last_letter = (word.length) - 1
-last_letter = word[number_of_last_letter]
+last_letter = word[-1]
 word[0] = last_letter
-word[number_of_last_letter] = first_letter
+word[-1] = first_letter
 puts word
