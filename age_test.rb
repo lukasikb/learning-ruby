05520fd89c51a992270c36fffaffd34e827f0de9
lukasikb <lukasikbed@onet.pl>
2021-04-07 22:21:33 +0200

    Checking the legal age

--- age_test.rb
+++ age_test.rb
@@ -1,7 +1,3 @@
 print "Input your age: "
 age = Integer(gets.chomp)
-if age < 18
-    puts "You are underage"
-else
-    puts "You are of legal age"
-end
+puts age < 18 ? "You are underage" : "You are of legal age"
